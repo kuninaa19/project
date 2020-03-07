3f4c04c3e7802ebc41977f390b5652ffdae11933
kuninaa19 <minchan0527@naver.com>
2020-03-08 00:20:46 +0900

    [update] href 위치 수정

--- auth/SignUp.php
+++ auth/SignUp.php
@@ -5,19 +5,12 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ITdream</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="../css/bootstrap-theme.css"/>
     <link rel="stylesheet" href="../css/bootstrap.css"/>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="../js/bootstrap.js"></script>
-    <link href="../css/style.css?afer" rel="stylesheet" type="text/css"/>
+    <link href="../css/style.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
 <br>
-<script src="loginRegExp.js"></script>
+<script src="../js/loginRegExp.js"></script>
 
 <div class="container">
     <div class="nav">
@@ -28,28 +21,34 @@
     <div class="box">
         <form method="post" name="sign_up" action="SignUp_ok.php">
             <fieldset>
-                <b class="txt_sign">아이디</b><br/><input type="text" class="info-form" name="id" onblur="validateId(this.value)">
+                <b class="txt_sign">아이디</b><br/><input type="text" class="info-form" name="id"
+                                                       onblur="validateId(this.value)">
                 <p>
                     <span class="error_box" id="idSpan"></span> <br/>
                 <p>
-                    <b class="txt_sign">비밀번호</b><br/><input type="password" class="info-form" name="pw" onblur="validatePassword(this.value)">
+                    <b class="txt_sign">비밀번호</b><br/><input type="password" class="info-form" name="pw"
+                                                            onblur="validatePassword(this.value)">
                 <p>
                     <span class="error_box" id="passwordSpan"></span><br/>
                 <p>
-                    <b class="txt_sign">비밀번호 재확인</b><br/><input type="password" class="info-form" name="pwcf" onblur="validatePassword2(this.value, pw.value)">
+                    <b class="txt_sign">비밀번호 재확인</b><br/><input type="password" class="info-form" name="pwcf"
+                                                                onblur="validatePassword2(this.value, pw.value)">
                 <p>
                     <span class="error_box" id="password2Span"></span><br/>
                 <p>
-                    <b class="txt_sign">이메일주소</b><br/><input type="text" class="info-form" name="email" onblur="validateEmail(this.value)">
+                    <b class="txt_sign">이메일주소</b><br/><input type="text" class="info-form" name="email"
+                                                             onblur="validateEmail(this.value)">
                 <p>
                     <span class="error_box" id="emailSpan"></span><br/>
                 <p>
-                    <b class="txt_sign">닉네임</b><br/><input type="text" class="info-form" name="nickname" onblur="validateNickname(this.value)">
+                    <b class="txt_sign">닉네임</b><br/><input type="text" class="info-form" name="nickname"
+                                                           onblur="validateNickname(this.value)">
                 <p>
                     <span class="error_box" id="nameSpan"></span><br/>
                 <p>
 
-                <p><input type="button" value="회원가입" class="button-form" id="button-color" onclick="placeOrder(this.form)"/></p>
+                <p><input type="button" value="회원가입" class="button-form" id="button-color"
+                          onclick="placeOrder(this.form)"/></p>
             </fieldset>
         </form>
     </div>
