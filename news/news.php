1bcf03721a33ae5b28ed39bb77328c0f07a55915
kuninaa19 <minchan0527@naver.com>
2020-03-08 23:50:53 +0900

    (수정) script 필요없는것들 제거

--- news/news.php
+++ news/news.php
@@ -78,13 +78,7 @@ include '../simple_html_dom.php';
         <meta charset='utf-8'>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>ITdream</title>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="../css/bootstrap-theme.css"/>
         <link rel="stylesheet" href="../css/bootstrap.css"/>
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-        <script type="text/javascript" src="../js/bootstrap.js"></script>
         <link  rel="stylesheet" type="text/css" href="../css/style.css"/>
 </head>
 <body>
@@ -99,10 +93,10 @@ include '../simple_html_dom.php';
         <div class="nav-item"><a href="../community/community.php?page=1&list=10">커뮤니티</a></div>
         <div class="nav-item"><a href="../notice/notice.php?page=1&list=10">공지사항</a></div>
         <div class="nav-item">
-        <?php include '../session_login.php' ?>
+        <?php include '../auth/session_login.php' ?>
         </div>
         <div class="nav-item">
-        <?php include '../session_signUp.php' ?>
+        <?php include '../auth/session_signUp.php' ?>
         </div>
       </div>
     </div>
