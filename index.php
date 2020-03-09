6a69cfdd9c5779f66406c8370d48f46e6d0aebe4
kuninaa19 <minchan0527@naver.com>
2020-03-09 19:07:17 +0900

    (수정) url 수정

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ if (isset($_COOKIE["id"]) && isset($_COOKIE["nickname"])) {
 //    $_SESSION['cnt_list'];
 
 } else {
+    echo $_COOKIE["chat"];
     session_start();
 //    $_SESSION['cnt_list'];
 }
@@ -57,6 +58,8 @@ foreach ($html2->find('h1.margin-bottom') as $article) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ITdream</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="css/bootstrap.css"/>
     <link href="css/style.css" rel="stylesheet" type="text/css"/>
     <style>
@@ -71,9 +74,11 @@ foreach ($html2->find('h1.margin-bottom') as $article) {
 <br>
 <div class="container">
     <div class="nav">
-        <div class="big-category"><a href="index.php"><img src="/smarteditor/upload/wefewfew.jpg" width="200px" height="50px"></a></div>
+        <div class="big-category"><a href="index.php"><img src="/smarteditor/upload/wefewfew.jpg" width="200px"
+                                                           height="50px"></a></div>
         <div class="nav-right-items">
-            <div class="nav-item"><a href="#" onclick="window.open('http://localhost:8080/auth', '대화방','width=570px height=670px'); return false">채팅</a>
+            <div class="nav-item"><a href="#"
+                                     onclick="window.open('http://localhost:8080/auth', '대화방','width=570px height=670px'); return false">채팅</a>
             </div>
             <div class="nav-item"><a href="review/review.php">리뷰</a></div>
             <div class="nav-item"><a href="news/news.php?page=1">뉴스</a></div>
@@ -137,7 +142,7 @@ foreach ($html2->find('h1.margin-bottom') as $article) {
     </div>
 
     <div class="nav_sub" style="margin-top:20px">
-        <div class="big-category"><a href="review/review_list.php?page=1&list=10"> 최근리뷰</a></div>
+        <div class="big-category"><a href="review/review.php?page=1&list=10"> 최근리뷰</a></div>
     </div>
     <p>
 
@@ -174,7 +179,7 @@ foreach ($html2->find('h1.margin-bottom') as $article) {
                             $width = '80%';
                             $height = '450px';
 
-                            echo "<div class='imgDiv'><a href='review/writing_rv.php?id=" . $row['id'] . "'><img src='http://img.youtube.com/vi/$id/maxresdefault.jpg' width='100%' height='100%'></div>";
+                            echo "<div class='imgDiv'><a href='review/review/boardPage.php?id=" . $row['id'] . "'><img src='http://img.youtube.com/vi/$id/maxresdefault.jpg' width='100%' height='100%'></div>";
                         }
                     } else {
                         // $imgAddress = str_replace('width="600px;"','',$row['sumnail']);
@@ -182,7 +187,7 @@ foreach ($html2->find('h1.margin-bottom') as $article) {
                         $imgAddress1 = str_replace('width', '', $row['sumnail']); // width 가리기
                         $imgAddress = str_replace('height', '', $imgAddress1); //height 가리기
 
-                        echo "<div class='imgDiv'><a href='review/writing_rv.php?id=" . $row['id'] . "'><img src=" . $imgAddress . "width='100%' height='100%'></div>";
+                        echo "<div class='imgDiv'><a href='review/review/boardPage.php?id=" . $row['id'] . "'><img src=" . $imgAddress . "width='100%' height='100%'></div>";
 
                     }
 
@@ -191,7 +196,7 @@ foreach ($html2->find('h1.margin-bottom') as $article) {
                     if ($row['sumnail'] != "") {
                         $imgAddress = str_replace('width="600px;"', '', $row['sumnail']);
 
-                        echo "<div class='imgDiv'><a href='review/writing_ubx.php?id=" . $row['id'] . "'><img src=" . $imgAddress . "width='100%' height='100%'></div>";
+                        echo "<div class='imgDiv'><a href='review/unboxing/boardPage.php?id=" . $row['id'] . "'><img src=" . $imgAddress . "width='100%' height='100%'></div>";
                     } else if ($row['video'] != "") {
                         $url = $row['video'];
                         if ($url != "") {
@@ -200,10 +205,10 @@ foreach ($html2->find('h1.margin-bottom') as $article) {
                             $width = '80%';
                             $height = '450px';
 
-                            echo "<div class='imgDiv'><a href='review/writing_ubx.php?id=" . $row['id'] . "'><img src='http://img.youtube.com/vi/$id/maxresdefault.jpg' width='100%' height='100%'></div>";
+                            echo "<div class='imgDiv'><a href='review/unboxing/boardPage.php?id=" . $row['id'] . "'><img src='http://img.youtube.com/vi/$id/maxresdefault.jpg' width='100%' height='100%'></div>";
                         }
                     } else {
-                        echo "<div class='imgDiv'><a href='writing_ubx.php?id=" . $row['id'] . "'><img src=" . $row['sumnail'] . "width='100%' height='100%'></div>";
+                        echo "<div class='imgDiv'><a href='review/unboxing/boardPage.php?id=" . $row['id'] . "'><img src=" . $row['sumnail'] . "width='100%' height='100%'></div>";
 
                     }
 
@@ -265,7 +270,7 @@ foreach ($html2->find('h1.margin-bottom') as $article) {
 
                     <!-- while문을통한 링크출력 -->
 
-                    <li class="index"><a href="community/writing.php?id=<?= $row['id'] ?>"><?= $row['title'] ?></li>
+                    <li class="index"><a href="community/boardPage.php?id=<?= $row['id'] ?>"><?= $row['title'] ?></li>
 
                     <?php
                 }
@@ -293,10 +298,7 @@ foreach ($html2->find('h1.margin-bottom') as $article) {
 
                 while ($row = mysqli_fetch_array($result)) {
                     ?>
-
-
-                    <li class="index"><a href="notice/writing.php?id=<?= $row['id'] ?>"><?= $row['title'] ?></li>
-
+                    <li class="index"><a href="notice/boardPage.php?id=<?= $row['id'] ?>"><?= $row['title'] ?></li>
                     <?php
                 }
                 ?>
