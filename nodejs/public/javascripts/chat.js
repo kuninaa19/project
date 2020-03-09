7b8cdc0f107eab375673393ca61914ce4cc5c2b2
kuninaa19 <minchan0527@naver.com>
2020-03-09 21:00:17 +0900

    (수정) url 세그먼트방식에서 방번호 값 가져오기

--- nodejs/public/javascripts/chat.js
+++ nodejs/public/javascripts/chat.js
@@ -1,26 +1,13 @@
 // URL get 정보만 가져오기위한 함수
-var getParameters = function (paramName) {
-    // 리턴값을 위한 변수 선언
-    var returnValue;
-
-    // 현재 URL 가져오기
-    var url = location.href;
-
-    // get 파라미터 값을 가져올 수 있는 ? 를 기점으로 slice 한 후 split 으로 나눔
-    var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&');
-
-    // 나누어진 값의 비교를 통해 paramName 으로 요청된 데이터의 값만 return
-    for (var i = 0; i < parameters.length; i++) {
-        var varName = parameters[i].split('=')[0];
-        if (varName.toUpperCase() == paramName.toUpperCase()) {
-            returnValue = parameters[i].split('=')[1];
-            return decodeURIComponent(returnValue);
-        }
-    }
+function getLastPath(url) {
+    var rLastPath = /\/([a-zA-Z0-9._]+)(?:\?.*)?$/;
+    return rLastPath.test(url) && RegExp.$1;
 };
 // 소켓io서버 객체화
 const socket = io();
-var room = getParameters('id');
+
+var room = getLastPath(window.location.pathname);
+
 socket.emit('joinRoom', room);
 
 //사람 현재인원표기
